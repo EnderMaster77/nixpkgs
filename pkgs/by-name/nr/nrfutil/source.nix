00ef9eb50e9bbef2e014068d99b3a06ba110a299
EnderMaster08 <javier.jgonzalez@protonmail.com>
2025-09-27 13:18:30 -0400

    Deleted a ton of packages. Hopefully this reduces file space and still lets the single package I use for this repo work.

--- pkgs/by-name/nr/nrfutil/source.nix
+++
@@ -1,7 +0,0 @@
-{
-  version = "7.13.0";
-  x86_64-linux = {
-    name = "x86_64-unknown-linux-gnu";
-    hash = "sha256-R3OF/340xEab+0zamfwvejY16fjy/3TrzMvQaBlVxHw=";
-  };
-}
--- pkgs/os-specific/linux/lvm2/2_03.nix
+++
@@ -1,4 +0,0 @@
-import ./common.nix {
-  version = "2.03.33";
-  hash = "sha256-vkur2KmG1zJ58edfux0zy0FVm3XCBjYReBv+uMLe8Tk=";
-}
